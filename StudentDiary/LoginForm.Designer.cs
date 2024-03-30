cc471881180634689f1e9e2073fbf4a97bc90102
Kazak_oqfcdqr <kazakovdanya@gmail.com>
2024-03-30 16:10:27 +0300

    Изменен размер окна с карточкой учителя

--- StudentDiary/LoginForm.Designer.cs
+++ StudentDiary/LoginForm.Designer.cs
@@ -38,18 +38,20 @@
             // 
             // LoginTextBox
             // 
-            this.LoginTextBox.Location = new System.Drawing.Point(215, 155);
+            this.LoginTextBox.Location = new System.Drawing.Point(161, 126);
+            this.LoginTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LoginTextBox.Name = "LoginTextBox";
-            this.LoginTextBox.Size = new System.Drawing.Size(339, 22);
+            this.LoginTextBox.Size = new System.Drawing.Size(255, 20);
             this.LoginTextBox.TabIndex = 0;
             // 
             // TitleLoginLabel
             // 
             this.TitleLoginLabel.AutoSize = true;
             this.TitleLoginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.TitleLoginLabel.Location = new System.Drawing.Point(169, 58);
+            this.TitleLoginLabel.Location = new System.Drawing.Point(127, 47);
+            this.TitleLoginLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TitleLoginLabel.Name = "TitleLoginLabel";
-            this.TitleLoginLabel.Size = new System.Drawing.Size(434, 32);
+            this.TitleLoginLabel.Size = new System.Drawing.Size(337, 26);
             this.TitleLoginLabel.TabIndex = 1;
             this.TitleLoginLabel.Text = "Электронный дневник студента";
             // 
@@ -57,26 +59,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(210, 123);
+            this.label1.Location = new System.Drawing.Point(158, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 29);
+            this.label1.Size = new System.Drawing.Size(64, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "Логин";
             // 
             // PasswordTextBox
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(215, 244);
+            this.PasswordTextBox.Location = new System.Drawing.Point(161, 198);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(339, 22);
+            this.PasswordTextBox.Size = new System.Drawing.Size(255, 20);
             this.PasswordTextBox.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(210, 201);
+            this.label2.Location = new System.Drawing.Point(158, 163);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.Size = new System.Drawing.Size(76, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "Пароль";
             // 
@@ -85,9 +90,10 @@
             this.EnterButton.BackColor = System.Drawing.SystemColors.HotTrack;
             this.EnterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.EnterButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.EnterButton.Location = new System.Drawing.Point(302, 341);
+            this.EnterButton.Location = new System.Drawing.Point(226, 277);
+            this.EnterButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.EnterButton.Name = "EnterButton";
-            this.EnterButton.Size = new System.Drawing.Size(166, 41);
+            this.EnterButton.Size = new System.Drawing.Size(124, 33);
             this.EnterButton.TabIndex = 5;
             this.EnterButton.Text = "Логин";
             this.EnterButton.UseVisualStyleBackColor = false;
@@ -95,16 +101,17 @@
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.EnterButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.PasswordTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TitleLoginLabel);
             this.Controls.Add(this.LoginTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LoginForm";
             this.Text = "Войти";
             this.ResumeLayout(false);
